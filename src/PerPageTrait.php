b8278d5b534dd716c8eef87a293fb976d073abb7
Luis Gustavo Santarosa Pinto <bolota_xd@hotmail.com>
2024-03-24 15:34:26 -0300

    Update PerPageTrait.php

--- src/PerPageTrait.php
+++ src/PerPageTrait.php
@@ -4,15 +4,16 @@ namespace GustavoSantarosa\PerPageTrait;
 
 trait PerPageTrait
 {
-    protected int $perPage = 0;
+    protected int $perPage;
 
-    public function getPerPage()
+    public function getPerPage(): int
     {
-        $this->perPage = request()->per_page ?? $this->perPage;
-        if ($this->perPage <= 0) {
-            $this->perPage = env('DEFAULT_PER_PAGE', 100);
-        } elseif ($this->perPage > env('MAX_PER_PAGE', 100)) {
-            $this->perPage = env('MAX_PER_PAGE', 100);
+        $this->perPage = request()->get('per_page', env('MAX_PER_PAGE'));
+
+        if($this->perPage > env('MAX_PER_PAGE')) {
+            $this->perPage = env('MAX_PER_PAGE');
+        } elseif($this->perPage < env('MIN_PER_PAGE')) {
+            $this->perPage = env('MIN_PER_PAGE');
         }
 
         return $this->perPage;
